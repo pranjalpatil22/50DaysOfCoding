85cd3fa5f79c904d5fe515115be43377b3ac0a8f
Pranjal Patil <pranjalpatilk22@gmail.com>
2026-06-29 17:53:44 +0530

    Add majorityElement function in Solution class
    Implement majority element algorithm using Boyer-Moore Voting.

---
+++ Day_10_7.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0,el = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(count == 0)
+            {
+                count++;
+                el = nums[i];
+            }
+            else if(nums[i] == el)
+            {
+                count++;
+            }else
+            {
+                count--;
+            }
+        }
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == el)
+            cnt++;
+        }
+
+        if(cnt > nums.size()/2)
+            return el;
+
+        return 0;
+
+    }
+};
