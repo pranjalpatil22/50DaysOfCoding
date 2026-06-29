ec1576e7268cce54facf0fc906fb8d0f78ec77c8
Pranjal Patil <pranjalpatilk22@gmail.com>
2026-06-29 07:08:53 +0530

    Add twoSum function in Solution class
    Implement twoSum function to find indices of two numbers that add up to a target.

---
+++ Day_10.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(mp.find(nums[i]) == mp.end())
+            {
+                mp[nums[i]]=i;
+            }
+        }
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(mp.find(target-nums[i]) != mp.end() && i != mp[target-nums[i]])
+            {
+                return {i,mp[target-nums[i]]};
+            }
+        }
+        return ans;
+    }
+};
