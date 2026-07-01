564fa03fb7e8812945e54312670bbe9cdd0b1449
Pranjal Patil <pranjalpatilk22@gmail.com>
2026-07-01 21:27:07 +0530

    Create Day_12_2.cpp

---
+++ Day_12_2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s;
+    cin >> s;
+    
+    if(s[0] == 'c' || s[3] == 'f')
+    {
+        cout << "yes";
+    }
+    else
+    cout << "no";
+}
