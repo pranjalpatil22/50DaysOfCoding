7354caeaaf404eb6c75cbe38ccef1aa0c856a0e4
Pranjal Patil <pranjalpatilk22@gmail.com>
2026-07-22 22:07:04 +0530

    Create Day_19.cpp

---
+++ Day_19.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int i;
+    cin >> i;
+    
+    if(i >= 3)
+    cout << "yes";
+    else 
+        cout << "no";
+
+}
